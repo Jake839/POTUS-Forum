bda9e88c676b68cd5d0bfd7e5e131c657dd324fd
Jake <jshap83@icloud.com>
2024-05-25 09:59:11 -0700

    undo 1-time scrolling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
-import "./controllers";
+import "@hotwired/turbo-rails"
+import "controllers"
 
 // For chartkick.com
-import "chartkick";
-import "Chart.bundle";
+import "chartkick"
+import "Chart.bundle"
 
 //Globally disable Turbo for forms 
-Turbo.setFormMode("off");
+Turbo.setFormMode("off")
 
 //= require jquery3
 
@@ -35,18 +35,13 @@ document.addEventListener("DOMContentLoaded", function () {
     var votedQuestionId = document.querySelector("#selected-question").getAttribute("data-voted-question-id");
     var scrollParameter = new URLSearchParams(window.location.search).get("scroll_to_top");
 
-    // Check if the session storage indicates that we need to scroll to the voted question
-    var scrollToVotedQuestion = sessionStorage.getItem("scroll_to_voted_question");
-
     if (scrollParameter === "true") {
         // Scroll to the top of the page
         $('html, body').animate({ scrollTop: 0 }, 1000); // Adjust the duration as needed
-         } else if (scrollToVotedQuestion && votedQuestionId) {
-         // Scroll to the selected question
-         $('html, body').animate({
+    } else if (votedQuestionId) {
+        // Scroll to the selected question's anchor or perform any other action
+        $('html, body').animate({
             scrollTop: $(`#question_${votedQuestionId}`).offset().top - 110
-         }, 1000); // Adjust the duration as needed
-        // Clear the flag so it doesn't scroll again on the next reload
-    //    sessionStorage.removeItem("scroll_to_voted_question");
+        }, 1000); // Adjust the duration as needed
     }
 });
