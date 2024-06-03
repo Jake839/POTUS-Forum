315da14142cbbb48360ba3228692948d61f1d53b
Jake <jshap83@icloud.com>
2024-06-03 14:57:09 -0700

    Add goatcounter script to application.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ puts "Loading Questions..."
 Question.destroy_all 
 question1 = Question.create(description: "Who will win the 2024 election?", date_string: "Thursday, May 30th, 2024", numerical_date: 5302024)
 question2 = Question.create(description: "Donald Trump is a convicted felon. He was found guilty on 34 counts of falsifying business records in the first degree. Was his trial rigged?", date_string: "Friday, May 31st, 2024", numerical_date: 5312024)
-=end 
 question3 = Question.create(description: "Will Donald Trump go to jail?", date_string: "Sunday, June 2nd, 2024", numerical_date: 6022024) 
+=end 
 
 
 puts "Loading Choices..."
@@ -31,7 +31,7 @@ choice4 = Choice.create(name: "Golden the cat", votes: 0, color: "#F7BD08", ques
 #Question 2 Choices
 choice5 = Choice.create(name: "Yes", votes: 0, color: "#D40C1C", question_id: question2.id)
 choice6 = Choice.create(name: "No", votes: 0, color: "#1226BA", question_id: question2.id)
-=end 
 #Question 3 Choices
 choice5 = Choice.create(name: "No", votes: 0, color: "#D40C1C", question_id: question3.id)
 choice6 = Choice.create(name: "Yes", votes: 0, color: "#1226BA", question_id: question3.id)
+=end 
