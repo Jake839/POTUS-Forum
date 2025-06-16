e395c5af2369ba438a4211820f20862c125e86d5
Jake <jshap83@icloud.com>
2025-06-16 04:25:56 -0700

    Update choice variable names in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,13 @@
 
 require 'open-uri' #for active storage 
 puts "Loading Questions..."
-=begin 
 Question.destroy_all 
 question1 = Question.create(description: "Who will win the 2024 election?", date_string: "Thursday, May 30th, 2024", numerical_date: 5302024)
 question2 = Question.create(description: "Donald Trump is a convicted felon. He was found guilty on 34 counts of falsifying business records in the first degree. Was his trial rigged?", date_string: "Friday, May 31st, 2024", numerical_date: 5312024)
 question3 = Question.create(description: "Will Donald Trump go to jail?", date_string: "Sunday, June 2nd, 2024", numerical_date: 6022024) 
-=end 
 question4 = Question.create(description: "Should Joe Biden take a cognitive test?", date_string: "Monday, June 17th, 2024", numerical_date: 6172024) 
 
-
 puts "Loading Choices..."
-=begin 
 Choice.destroy_all 
 #Question 1 Choices
 choice1 = Choice.create(name: "Donald Trump", votes: 0, color: "#D40C1C", question_id: question1.id)
@@ -33,9 +29,8 @@ choice4 = Choice.create(name: "Golden the cat", votes: 0, color: "#F7BD08", ques
 choice5 = Choice.create(name: "Yes", votes: 0, color: "#D40C1C", question_id: question2.id)
 choice6 = Choice.create(name: "No", votes: 0, color: "#1226BA", question_id: question2.id)
 #Question 3 Choices
-choice5 = Choice.create(name: "No", votes: 0, color: "#D40C1C", question_id: question3.id)
-choice6 = Choice.create(name: "Yes", votes: 0, color: "#1226BA", question_id: question3.id)
-=end 
+choice7 = Choice.create(name: "No", votes: 0, color: "#D40C1C", question_id: question3.id)
+choice8 = Choice.create(name: "Yes", votes: 0, color: "#1226BA", question_id: question3.id)
 #Question 4 Choices
-choice5 = Choice.create(name: "Yes", votes: 0, color: "#D40C1C", question_id: question4.id)
-choice6 = Choice.create(name: "No", votes: 0, color: "#1226BA", question_id: question4.id)
+choice9 = Choice.create(name: "Yes", votes: 0, color: "#D40C1C", question_id: question4.id)
+choice10 = Choice.create(name: "No", votes: 0, color: "#1226BA", question_id: question4.id)
